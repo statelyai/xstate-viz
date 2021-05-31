1642a0881897534733a1aac8b233b66df6676d4b
David Khourshid <davidkpiano@gmail.com>
2021-05-31 12:14:38 -0400

    Improve panning performance

---
+++ src/CanvasContainer.tsx
@@ -0,0 +1,28 @@
+import { useMachine } from '@xstate/react';
+import * as React from 'react';
+import { canvasMachine, canvasModel } from './App';
+
+export const CanvasContainer: React.FC = ({ children }) => {
+  const [state, send] = useMachine(canvasMachine);
+
+  return (
+    <div
+      data-panel="viz"
+      onWheel={(e) => {
+        send(canvasModel.events.PAN(e.deltaX, e.deltaY));
+      }}
+    >
+      <div>
+        <button onClick={() => send('ZOOM.OUT')}>-</button>
+        <button onClick={() => send('ZOOM.IN')}>+</button>
+      </div>
+      <div
+        style={{
+          transform: `translate(${state.context.pan.dx}px, ${state.context.pan.dy}px) scale(${state.context.zoom})`,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
--- src/CanvasPanel.tsx
+++ src/CanvasPanel.tsx
@@ -1,48 +1,16 @@
 import { DirectedGraphNode } from '@xstate/graph';
-import { useMachine } from '@xstate/react';
 import * as React from 'react';
-import { canvasMachine, canvasModel, SimulationContext } from './App';
+import { CanvasContainer } from './CanvasContainer';
 import { Graph } from './Graph';
 import { MachineViz } from './MachineViz';
 
 export const CanvasPanel: React.FC<{ digraph: DirectedGraphNode }> = ({
   digraph,
 }) => {
-  const simService = React.useContext(SimulationContext);
-  const [state, send] = useMachine(canvasMachine);
-
   return (
-    <div
-      data-panel="viz"
-      onWheel={(e) => {
-        send(canvasModel.events.PAN(e.deltaX, e.deltaY));
-      }}
-    >
-      <div>
-        <button onClick={() => send('ZOOM.OUT')}>-</button>
-        <button onClick={() => send('ZOOM.IN')}>+</button>
-        <button
-          onClick={() =>
-            simService.send({
-              type: 'EVENT',
-              event: { type: 'NEXT' },
-            })
-          }
-        >
-          NEXT
-        </button>
-        <button onClick={() => simService.send('MACHINE.UPDATE')}>
-          MACHINE
-        </button>
-      </div>
-      <div
-        style={{
-          transform: `translate(${state.context.pan.dx}px, ${state.context.pan.dy}px) scale(${state.context.zoom})`,
-        }}
-      >
-        <MachineViz digraph={digraph} />
-        <Graph digraph={digraph} />
-      </div>
-    </div>
+    <CanvasContainer>
+      <MachineViz digraph={digraph} />
+      <Graph digraph={digraph} />
+    </CanvasContainer>
   );
 };
