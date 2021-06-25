ddabd5fc78caa603ed21c2ee102ef8ef3ba2d884
David Khourshid <davidkpiano@gmail.com>
2021-06-25 13:38:09 -0400

    Fix viz not updating from code

--- src/simulationMachine.tsx
+++ src/simulationMachine.tsx
@@ -60,7 +60,7 @@ export const createSimulationMachine = (
     entry: assign({ notifRef: () => spawn(notifMachine) }),
     invoke: {
       id: 'services',
-      src: (ctx) => (sendBack) => {
+      src: () => (sendBack) => {
         devTools.onRegister((service) => {
           sendBack(simModel.events['SERVICE.REGISTER'](service));
 
@@ -88,7 +88,7 @@ export const createSimulationMachine = (
             target: 'active',
             actions: [
               simModel.assign((_, e) => ({
-                machineIndex: (e as any).data[0],
+                machineIndex: (e as any).data.length - 1,
                 machines: (e as any).data,
               })) as any,
             ],
