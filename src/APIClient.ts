b55a87f21d78c3bb064d891596b7f3e31c2c4c99
Farzad Yousefzadeh <farskid@gmail.com>
2021-06-25 15:14:55 +0300

    adds ebv variables

--- src/APIClient.ts
+++ src/APIClient.ts
@@ -1,9 +1,5 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-// TODO: keep in env variable
-const APIKEY_ANON_PUBLIC =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzkzNTUzOCwiZXhwIjoxOTM5NTExNTM4fQ.srHSjKsgKFPREW6M0jGbFrvIWsmI0XfSTthBTFOZXI8';
-
 interface Client extends SupabaseClient {
   initialized: boolean;
 }
@@ -12,12 +8,14 @@ export let client: Client = {
   initialized: false,
 } as Client;
 
+console.log(process.env);
+
 export const initAPIClient = () => {
   client = Object.assign(
     {},
     createClient(
-      'https://owxhbmonbfrgnegwprcq.supabase.co',
-      APIKEY_ANON_PUBLIC,
+      process.env.REACT_APP_SUPABASE_API_URL as string,
+      process.env.REACT_APP_SUPABASE_ANON_API_KEY as string,
     ),
     { initialized: true },
   );
--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,12 @@
 // 1. import `extendTheme` function
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 // 2. Add your color mode config
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
 
 // 3. extend the theme
 export const theme = extendTheme({
-  config: {
-    initialColorMode: 'dark',
-    useSystemColorMode: true,
-  },
+  config,
 });
