f7c77a7a0d2fbf8acb888a7e28fd977cef5b9b30
Farzad Yousefzadeh <farskid@gmail.com>
2021-07-10 20:43:58 +0300

    log preview errors to the console

--- src/StateNodeViz.tsx
+++ src/StateNodeViz.tsx
@@ -66,7 +66,9 @@ export const StateNodeViz: React.FC<{
     // Catch exceptions thrown by invalid actions or guards on the transition event
     try {
       return simMachine?.transition(simState, state.context.previewEvent);
-    } catch (err) {}
+    } catch (err) {
+      console.error(err);
+    }
   }, [state, simState, simMachine]);
 
   useEffect(() => {
