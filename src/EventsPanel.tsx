d99c48bb058fa6c1d10522281f58cfec15ca60cf
Farzad Yousefzadeh <farskid@gmail.com>
2021-07-14 13:49:10 +0300

    events panel upgrade: sorting by timestamp, show/hide builtin events

--- src/EventsPanel.tsx
+++ src/EventsPanel.tsx
@@ -8,14 +8,24 @@ import {
   Td,
   Thead,
   Th,
+  Button,
+  Switch,
+  FormLabel,
 } from '@chakra-ui/react';
-import { useActor } from '@xstate/react';
-import React, { useMemo, useState } from 'react';
+import { useActor, useMachine } from '@xstate/react';
+import React, { useEffect, useMemo, useState } from 'react';
 import ReactJson from 'react-json-view';
 import { useSimulation } from './SimulationContext';
 import { format } from 'date-fns';
 import { SimEvent } from './simulationMachine';
-import { ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  ChevronUpIcon,
+} from '@chakra-ui/icons';
+import { createMachine } from 'xstate';
+import { createModel } from 'xstate/lib/model';
+import { send } from 'xstate/lib/actions';
 
 const EventConnection: React.FC<{ event: SimEvent }> = ({ event }) => {
   return (
@@ -34,12 +44,121 @@ const stickyProps = {
   zIndex: 1,
 };
 
+const sortByCriteria = {
+  ASC: (events: SimEvent[]) =>
+    events.sort((a, b) => (a.timestamp < b.timestamp ? 1 : -1)),
+  DESC: (events: SimEvent[]) =>
+    events.sort((a, b) => (a.timestamp > b.timestamp ? 1 : -1)),
+};
+type SORT_CRITERIA = 'ASC' | 'DESC';
+const eventsModel = createModel(
+  {
+    sortCriteria: null as SORT_CRITERIA | null,
+    filterKeyword: '',
+    showBuiltins: false,
+    rawEvents: [] as SimEvent[],
+    finalEvents: [] as SimEvent[],
+  },
+  {
+    events: {
+      SORT_BY_TIMESTAMP: (sortCriteria: SORT_CRITERIA) => ({ sortCriteria }),
+      FILTER: (keyword: string) => ({ keyword }),
+      TOGGLE_BUILTIN_EVENTS: (showBuiltins: boolean) => ({ showBuiltins }),
+      EVENTS_UPDATED: (events: SimEvent[]) => ({ events }),
+      INTERNAL_UPDATE: () => ({}),
+    },
+  },
+);
+const eventsMachine = createMachine<typeof eventsModel>({
+  initial: 'raw',
+  context: eventsModel.initialContext,
+  states: {
+    raw: {},
+    modified: {},
+  },
+  on: {
+    INTERNAL_UPDATE: {
+      actions: [
+        eventsModel.assign((ctx) => {
+          let finalEvents = ctx.rawEvents;
+          console.log(ctx);
+          if (!ctx.showBuiltins) {
+            finalEvents = finalEvents.filter(
+              (event) => !event.name.startsWith('xstate.'),
+            );
+          }
+          if (ctx.filterKeyword) {
+            finalEvents = finalEvents.filter((evt) =>
+              evt.name.toUpperCase().includes(ctx.filterKeyword.toUpperCase()),
+            );
+          }
+          if (ctx.sortCriteria) {
+            finalEvents = sortByCriteria[ctx.sortCriteria](finalEvents.slice());
+          }
+          return {
+            ...ctx,
+            finalEvents,
+          };
+        }),
+      ],
+    },
+    SORT_BY_TIMESTAMP: {
+      target: 'modified',
+      actions: [
+        eventsModel.assign((_, e) => ({
+          sortCriteria: e.sortCriteria,
+        })),
+        send('INTERNAL_UPDATE'),
+      ],
+    },
+    FILTER: {
+      target: 'modified',
+      actions: [
+        eventsModel.assign((_, e) => ({
+          filterKeyword: e.keyword,
+        })),
+        send('INTERNAL_UPDATE'),
+      ],
+    },
+    EVENTS_UPDATED: {
+      actions: [
+        eventsModel.assign((_, e) => ({
+          rawEvents: e.events,
+        })),
+        send('INTERNAL_UPDATE'),
+      ],
+    },
+    TOGGLE_BUILTIN_EVENTS: {
+      target: 'modified',
+      actions: [
+        eventsModel.assign((_, e) => ({
+          showBuiltins: e.showBuiltins,
+        })),
+        send('INTERNAL_UPDATE'),
+      ],
+    },
+  },
+});
+
 export const EventsPanel: React.FC = () => {
   const [state] = useActor(useSimulation());
-  const events = useMemo(() => {
-    return state.context.events.filter((event) => event.name !== 'xstate.init');
+  const rawEvents = useMemo(() => {
+    return state.context!.events;
   }, [state]);
-  const [filter, setFilter] = useState('');
+
+  const [eventsState, sendToEventsMachine] = useMachine(
+    eventsMachine.withContext({
+      ...eventsModel.initialContext,
+      rawEvents: rawEvents,
+    }),
+  );
+
+  useEffect(() => {
+    sendToEventsMachine({
+      type: 'EVENTS_UPDATED',
+      events: rawEvents,
+    });
+  }, [rawEvents, sendToEventsMachine]);
 
   return (
     <Box
@@ -48,11 +167,31 @@ export const EventsPanel: React.FC = () => {
       gridRowGap="2"
       height="100%"
     >
-      <Box>
+      <Box display="flex" justifyContent="flex-end" alignItems="center">
         <Input
           placeholder="Filter events"
-          onChange={(e) => setFilter(e.target.value)}
+          type="search"
+          onChange={(e) => {
+            sendToEventsMachine({ type: 'FILTER', keyword: e.target.value });
+          }}
+          marginRight="auto"
+          width="40%"
         />
+
+        <Box display="flex" alignItems="center">
+          <FormLabel marginBottom="0" marginRight="1" htmlFor="builtin-toggle">
+            Show built-in events
+          </FormLabel>
+          <Switch
+            id="builtin-toggle"
+            onChange={(e) => {
+              sendToEventsMachine({
+                type: 'TOGGLE_BUILTIN_EVENTS',
+                showBuiltins: e.target.checked,
+              });
+            }}
+          />
+        </Box>
       </Box>
       <Box overflowY="auto">
         <Table width="100%">
@@ -62,12 +201,57 @@ export const EventsPanel: React.FC = () => {
                 Event type
               </Th>
               <Th {...stickyProps}>To</Th>
-              <Th {...stickyProps}>Time</Th>
+              <Th {...stickyProps} whiteSpace="nowrap">
+                Time
+                <Box
+                  display="inline-flex"
+                  flexDirection="column"
+                  verticalAlign="middle"
+                  marginLeft="1"
+                >
+                  <Button
+                    variant="unstyled"
+                    size="xs"
+                    title="sort by timestamp descending"
+                    bg={
+                      eventsState.context.sortCriteria === 'DESC'
+                        ? 'var(--chakra-colors-gray-700)'
+                        : undefined
+                    }
+                    onClick={() => {
+                      sendToEventsMachine({
+                        type: 'SORT_BY_TIMESTAMP',
+                        sortCriteria: 'DESC',
+                      });
+                    }}
+                  >
+                    <ChevronUpIcon />
+                  </Button>
+                  <Button
+                    variant="unstyled"
+                    size="xs"
+                    title="sort by timestamp ascending"
+                    bg={
+                      eventsState.context.sortCriteria === 'ASC'
+                        ? 'var(--chakra-colors-gray-700)'
+                        : undefined
+                    }
+                    onClick={() => {
+                      sendToEventsMachine({
+                        type: 'SORT_BY_TIMESTAMP',
+                        sortCriteria: 'ASC',
+                      });
+                    }}
+                  >
+                    <ChevronDownIcon />
+                  </Button>
+                </Box>
+              </Th>
             </Tr>
           </Thead>
           <Tbody>
-            {events.map((event, i) => {
-              return <EventRow event={event} filter={filter} key={i} />;
+            {eventsState.context.finalEvents.map((event, i) => {
+              return <EventRow event={event} key={i} />;
             })}
           </Tbody>
         </Table>
@@ -76,21 +260,12 @@ export const EventsPanel: React.FC = () => {
   );
 };
 
-const EventRow: React.FC<{ event: SimEvent; filter: string }> = ({
-  event,
-  filter,
-}) => {
-  console.log(event);
-  const matchesFilter = event.name.toUpperCase().includes(filter.toUpperCase());
+const EventRow: React.FC<{ event: SimEvent }> = ({ event }) => {
   const [show, setShow] = useState(false);
 
   return (
     <>
-      <Tr
-        hidden={!matchesFilter || undefined}
-        cursor="pointer"
-        onClick={() => setShow(!show)}
-      >
+      <Tr cursor="pointer" onClick={() => setShow(!show)}>
         <Td>
           {show ? <ChevronDownIcon /> : <ChevronRightIcon />}
           {event.name}
@@ -100,7 +275,7 @@ const EventRow: React.FC<{ event: SimEvent; filter: string }> = ({
         </Td>
         <Td color="gray.500">{format(event.timestamp, 'hh:mm:ss')}</Td>
       </Tr>
-      {matchesFilter && show ? (
+      {show ? (
         <Tr>
           <Td colSpan={3}>
             <ReactJson src={event.data} theme="monokai" />
