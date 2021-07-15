1c61d80563bb8fe94f5633e3869e2f08d525f6ee
David Khourshid <davidkpiano@gmail.com>
2021-07-15 09:53:57 -0400

    Dummy test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 // import App from './App';
 
-// describe('<App>', () => {
-//   it.skip('renders app', () => {
-//     const { getByTestId } = render(<App />);
-//     const app = getByTestId('app');
-//     expect(document.body.contains(app)).toBeTruthy();
-//   });
-// });
+describe('<App>', () => {
+  it('testing works', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
