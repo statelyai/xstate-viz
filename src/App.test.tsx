3926b584ba74198976a549857c4f7ba4bc35be58
David Khourshid <davidkpiano@gmail.com>
2021-07-15 08:47:35 -0400

    Comment out tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { expect } from 'chai';
-import App from './App';
+// import App from './App';
 
-describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
-  });
-});
+// describe('<App>', () => {
+//   it.skip('renders app', () => {
+//     const { getByTestId } = render(<App />);
+//     const app = getByTestId('app');
+//     expect(document.body.contains(app)).toBeTruthy();
+//   });
+// });
