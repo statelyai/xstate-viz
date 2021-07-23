b7e773b7f8a8a2ec4487513c6c5dd762d0a5ba3a
Matt Pocock <mpocock@me.com>
2021-07-23 12:08:04 +0100

    Update cypress/support/commands.ts

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -40,15 +40,3 @@ Cypress.Commands.add('interceptGraphQL', (data) => {
     },
   });
 });
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
