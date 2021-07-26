2bb169ec3cd29a3b3779d84ed495aba92bd9e34c
Matt Pocock <mpocock@me.com>
2021-07-26 09:48:23 +0100

    Update src/authContext.tsx
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- src/authContext.tsx
+++ src/authContext.tsx
@@ -3,4 +3,4 @@ import { authMachine } from './authMachine';
 import { createRequiredContext } from './utils';
 
 export const [AuthProvider, useAuth] =
-  createRequiredContext<InterpreterFrom<typeof authMachine>>('Simulation');
+  createRequiredContext<InterpreterFrom<typeof authMachine>>('Auth');
