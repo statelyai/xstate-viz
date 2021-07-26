e4e085bd88fbbb703f39fc241c3a534d9d5e8fab
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-07-26 16:47:48 +0200

    Separate canvas buttons with a separator

--- src/CanvasPanel.tsx
+++ src/CanvasPanel.tsx
@@ -14,6 +14,10 @@ import { Graph } from './Graph';
 import { useSimulation } from './SimulationContext';
 import { theme } from './theme';
 
+const ButtonSeparator = () => (
+  <Box backgroundColor="whiteAlpha.400" width={0.5} height="60%" marginX={1} />
+);
+
 export const CanvasPanel: React.FC<{
   digraph: DirectedGraphNode;
 }> = ({ digraph }) => {
@@ -22,39 +26,38 @@ export const CanvasPanel: React.FC<{
 
   return (
     <Box display="grid" gridTemplateRows="auto 1fr">
-      <Box zIndex={1} bg="black">
-        <ChakraProvider theme={theme}>
-          <Box display="flex" justifyContent="space-between">
-            <ButtonGroup size="sm" spacing={2} padding={2}>
-              <IconButton
-                aria-label="Zoom out"
-                title="Zoom out"
-                icon={<MinusIcon />}
-                onClick={() => canvasService.send('ZOOM.OUT')}
-              />
-              <IconButton
-                aria-label="Zoom in"
-                title="Zoom in"
-                icon={<AddIcon />}
-                onClick={() => canvasService.send('ZOOM.IN')}
-              />
-              <IconButton
-                aria-label="Reset canvas"
-                title="Reset canvas"
-                icon={<RepeatIcon />}
-                onClick={() => canvasService.send('POSITION.RESET')}
-              />
-            </ButtonGroup>
-            <Button
-              size="sm"
-              margin={2}
-              onClick={() => simService.send('MACHINES.RESET')}
-            >
-              {'Reset machine'}
-            </Button>
-          </Box>
-        </ChakraProvider>
-      </Box>
+      <ChakraProvider theme={theme}>
+        <Box zIndex={1} bg="black" display="flex" alignItems="center">
+          <ButtonGroup size="sm" spacing={2} padding={2}>
+            <IconButton
+              aria-label="Zoom out"
+              title="Zoom out"
+              icon={<MinusIcon />}
+              onClick={() => canvasService.send('ZOOM.OUT')}
+            />
+            <IconButton
+              aria-label="Zoom in"
+              title="Zoom in"
+              icon={<AddIcon />}
+              onClick={() => canvasService.send('ZOOM.IN')}
+            />
+            <IconButton
+              aria-label="Reset canvas"
+              title="Reset canvas"
+              icon={<RepeatIcon />}
+              onClick={() => canvasService.send('POSITION.RESET')}
+            />
+          </ButtonGroup>
+          <ButtonSeparator />
+          <Button
+            size="sm"
+            margin={2}
+            onClick={() => simService.send('MACHINES.RESET')}
+          >
+            {'Reset machine'}
+          </Button>
+        </Box>
+      </ChakraProvider>
       <CanvasContainer>
         <Graph digraph={digraph} />
       </CanvasContainer>
