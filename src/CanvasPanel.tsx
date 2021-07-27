cb20982a0cef436a7460d87ddab6e6c1500b0ac0
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-07-27 11:23:27 +0200

    Repurpose existing Reset button to reset position (#79)
    * Repurpose existing Reset button to reset position
    
    * Separate canvas buttons with a separator

--- src/CanvasPanel.tsx
+++ src/CanvasPanel.tsx
@@ -1,5 +1,11 @@
 import { AddIcon, MinusIcon, RepeatIcon } from '@chakra-ui/icons';
-import { Box, ButtonGroup, ChakraProvider, IconButton } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  ChakraProvider,
+  IconButton,
+} from '@chakra-ui/react';
 import React from 'react';
 import { CanvasContainer } from './CanvasContainer';
 import { useCanvas } from './CanvasContext';
@@ -7,6 +13,10 @@ import { DirectedGraphNode } from './directedGraph';
 import { Graph } from './Graph';
 import { useSimulation } from './SimulationContext';
 import { theme } from './theme';
+
+const ButtonSeparator = () => (
+  <Box backgroundColor="whiteAlpha.400" width={0.5} height="60%" marginX={1} />
+);
 
 export const CanvasPanel: React.FC<{
   digraph: DirectedGraphNode;
@@ -16,8 +26,8 @@ export const CanvasPanel: React.FC<{
 
   return (
     <Box display="grid" gridTemplateRows="auto 1fr">
-      <Box zIndex={1} bg="black">
-        <ChakraProvider theme={theme}>
+      <ChakraProvider theme={theme}>
+        <Box zIndex={1} bg="black" display="flex" alignItems="center">
           <ButtonGroup size="sm" spacing={2} padding={2}>
             <IconButton
               aria-label="Zoom out"
@@ -32,14 +42,22 @@ export const CanvasPanel: React.FC<{
               onClick={() => canvasService.send('ZOOM.IN')}
             />
             <IconButton
-              aria-label="Reset"
-              title="Reset"
+              aria-label="Reset canvas"
+              title="Reset canvas"
               icon={<RepeatIcon />}
-              onClick={() => simService.send('MACHINES.RESET')}
+              onClick={() => canvasService.send('POSITION.RESET')}
             />
           </ButtonGroup>
-        </ChakraProvider>
-      </Box>
+          <ButtonSeparator />
+          <Button
+            size="sm"
+            margin={2}
+            onClick={() => simService.send('MACHINES.RESET')}
+          >
+            {'Reset machine'}
+          </Button>
+        </Box>
+      </ChakraProvider>
       <CanvasContainer>
         <Graph digraph={digraph} />
       </CanvasContainer>
