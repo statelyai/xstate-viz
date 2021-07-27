b95b2e32ec4a92eba6a222fbd7b3b366de5767cb
David Khourshid <davidkpiano@gmail.com>
2021-07-27 06:35:10 -0400

    Add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import './monacoPatch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import App from './App';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
