cedb52d86b1c8c65fd8f1c80fc8db685714652c6
Matt Pocock <mattpocockvoice@gmail.com>
2021-07-29 09:18:19 +0100

    Re-added line deleted by mistake

--- src/EditorWithXStateImports.tsx
+++ src/EditorWithXStateImports.tsx
@@ -115,6 +115,7 @@ export const EditorWithXStateImports = (
               keybindings: [KeyMod.CtrlCmd | KeyCode.KEY_S],
               run: () => {
                 props.onSave?.();
+                editor.getAction('editor.action.formatDocument').run();
               },
             });
 
