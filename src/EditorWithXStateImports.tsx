aa9dbd86580771dfe9552102ada0a1ad003dae52
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-07-30 12:23:11 +0200

    Add support for models in the editor and improve type acquisition (#95)
    * Add support for models in the editor and improve type acquisition
    
    * Use safe memory-web-storage in the typeAcquisition file
    
    * Fixup autoimport specifiers
    
    * Always acquire types for supported xstate modules
    
    * Add comment about import to import type fixup
    
    * Add additional comments

--- src/EditorWithXStateImports.tsx
+++ src/EditorWithXStateImports.tsx
@@ -1,10 +1,11 @@
 import { ClassNames } from '@emotion/react';
-import Editor, { OnMount } from '@monaco-editor/react';
-import { KeyCode, KeyMod } from 'monaco-editor';
+import Editor, { OnMount, Monaco } from '@monaco-editor/react';
+import { KeyCode, KeyMod, editor } from 'monaco-editor';
 import { SpinnerWithText } from './SpinnerWithText';
 import { format } from 'prettier/standalone';
 import tsParser from 'prettier/parser-typescript';
 import { setMonacoTheme } from './setMonacoTheme';
+import { detectNewImportsToAcquireTypeFor } from './typeAcquisition';
 import { uniq } from './utils';
 import { SourceProvider } from './types';
 
@@ -60,6 +61,42 @@ interface EditorWithXStateImportsProps {
   defaultValue?: string;
 }
 
+// based on the logic here: https://github.com/microsoft/TypeScript-Website/blob/103f80e7490ad75c34917b11e3ebe7ab9e8fc418/packages/sandbox/src/index.ts
+const withTypeAcquisition = (
+  editor: editor.IStandaloneCodeEditor,
+  monaco: Monaco,
+): editor.IStandaloneCodeEditor => {
+  const addLibraryToRuntime = (code: string, path: string) => {
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(code, path);
+    const uri = monaco.Uri.file(path);
+    if (monaco.editor.getModel(uri) === null) {
+      monaco.editor.createModel(code, 'javascript', uri);
+    }
+  };
+
+  const textUpdated = (code = editor.getModel()!.getValue()) => {
+    detectNewImportsToAcquireTypeFor(
+      code,
+      addLibraryToRuntime,
+      window.fetch.bind(window),
+      { logger: console },
+    );
+  };
+
+  // to enable type acquisition for any module we can introduce a deboouncer like here:
+  // https://github.com/microsoft/TypeScript-Website/blob/97a97d460d64c3c363878f11db198d0027885d8d/packages/sandbox/src/index.ts#L204-L213
+
+  textUpdated(
+    // those are modules that we actually allow when we load the code at runtime
+    // this "prefetches" the types for those modules so the autoimport feature can kick in asap for them
+    ['xstate', 'xstate/lib/model', 'xstate/lib/actions']
+      .map((specifier) => `import '${specifier}'`)
+      .join('\n'),
+  );
+
+  return editor;
+};
+
 export const EditorWithXStateImports = (
   props: EditorWithXStateImportsProps,
 ) => {
@@ -99,15 +136,6 @@ export const EditorWithXStateImports = (
                 monaco.languages.typescript.ModuleResolutionKind.NodeJs,
               strict: true,
             });
-
-            const indexFile = await fetch(`/xstate.d.ts.txt`).then((res) =>
-              res.text(),
-            );
-
-            monaco.languages.typescript.typescriptDefaults.addExtraLib(
-              indexFile,
-              'file:///node_modules/xstate/index.d.ts',
-            );
 
             // Prettier to format
             // Ctrl/CMD + Enter to visualize
@@ -168,7 +196,7 @@ export const EditorWithXStateImports = (
               }
             }
 
-            props.onMount?.(editor, monaco);
+            props.onMount?.(withTypeAcquisition(editor, monaco), monaco);
           }}
         />
       )}
