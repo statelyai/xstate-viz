b8c926833f039a218010a923085fe8f1ba0c9968
Matt Pocock <mattpocockvoice@gmail.com>
2021-08-03 15:33:51 +0100

    Added a vercel-ignore script

---
+++ vercel-ignore.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script tells vercel which branches to build,
+# and which to ignore
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ || "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
