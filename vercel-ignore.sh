fc21ef08ffd0b29f43346a92e227e3ff3f46c55a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-08-10 12:27:47 +0200

    Fixed an issue with builds being skipped when multiple refs point to a single commit

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -4,14 +4,15 @@
 # and which to ignore
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  # Proceed with the build
-    echo "✅ - Build can proceed"
+if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
+  echo "✅ - Build can proceed"
   exit 1;
-
+elif [[ "$VERCEL_GIT_COMMIT_SHA" == "$(git rev-parse --short main)" ]] ; then
+  echo "✅ - Build can proceed"
+  exit 1;
 else
-  # Don't build
   echo "🛑 - Build cancelled"
   exit 0;
 fi
