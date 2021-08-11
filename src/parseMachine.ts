4ac5756bbd23b7808e0246b8adf65fe8cdff7226
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-08-11 11:46:10 +0200

    Sandbox editor's code execution in a realm

--- src/parseMachine.ts
+++ src/parseMachine.ts
@@ -2,11 +2,11 @@ import * as XState from 'xstate';
 import * as XStateModel from 'xstate/lib/model';
 import * as XStateActions from 'xstate/lib/actions';
 import { StateNode } from 'xstate';
+import realmsShim from 'realms-shim';
+
+const realm = realmsShim.makeRootRealm();
 
 export function parseMachines(sourceJs: string): Array<StateNode> {
-  // eslint-disable-next-line no-new-func
-  const makeMachine = new Function('exports', 'require', sourceJs);
-
   const machines: Array<StateNode> = [];
 
   const createMachineCapturer =
@@ -17,31 +17,43 @@ export function parseMachines(sourceJs: string): Array<StateNode> {
       return machine;
     };
 
-  makeMachine({}, (sourcePath: string) => {
-    switch (sourcePath) {
-      case 'xstate':
-        return {
-          ...XState,
-          createMachine: createMachineCapturer(XState.createMachine),
-          Machine: createMachineCapturer(XState.Machine),
-        };
-      case 'xstate/lib/actions':
-        return XStateActions;
-      case 'xstate/lib/model':
-        const { createModel } = XStateModel;
-        return {
-          ...XStateModel,
-          createModel(initialContext: any, creators: any) {
-            const model = createModel(initialContext, creators);
-            return {
-              ...model,
-              createMachine: createMachineCapturer(model.createMachine),
-            };
-          },
-        };
-      default:
-        throw new Error(`External module ("${sourcePath}") can't be used.`);
-    }
+  realm.evaluate(sourceJs, {
+    // we just allow for export statements to be used in the source code
+    // we don't have any use for the exported values so we just mock the `exports` object
+    exports: {},
+    require: (sourcePath: string) => {
+      switch (sourcePath) {
+        case 'xstate':
+          return {
+            ...XState,
+            createMachine: createMachineCapturer(XState.createMachine),
+            Machine: createMachineCapturer(XState.Machine),
+          };
+        case 'xstate/lib/actions':
+          return XStateActions;
+        case 'xstate/lib/model':
+          const { createModel } = XStateModel;
+          return {
+            ...XStateModel,
+            createModel(initialContext: any, creators: any) {
+              const model = createModel(initialContext, creators);
+              return {
+                ...model,
+                createMachine: createMachineCapturer(model.createMachine),
+              };
+            },
+          };
+        default:
+          throw new Error(`External module ("${sourcePath}") can't be used.`);
+      }
+    },
+    // users might want to access `console` in the sandboxed env
+    console: {
+      error: console.error,
+      info: console.info,
+      log: console.log,
+      warn: console.warn,
+    },
   });
 
   return machines;
