7b19bf4202b83a81e9ddb74fd1d0c0120cdeb5c6
David Khourshid <davidkpiano@gmail.com>
2021-08-11 13:51:18 -0400

    Footer styling

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,15 +1,30 @@
 import { Box } from '@chakra-ui/layout';
+import { ChakraProvider, Text } from '@chakra-ui/react';
 import { useActor } from '@xstate/react';
 import React from 'react';
 import { useSimulation } from './SimulationContext';
+import { theme } from './theme';
 
 export const Footer: React.FC = () => {
   const sim = useSimulation();
   const [state] = useActor(sim);
 
   return (
-    <Box padding="8" background="black" gridArea="footer">
-      {JSON.stringify(state.value)}
-    </Box>
+    <ChakraProvider theme={theme}>
+      <Box
+        padding="1"
+        paddingInline="2"
+        background="gray.900"
+        gridArea="footer"
+      >
+        <Text
+          fontSize="sm"
+          title={JSON.stringify(state.value, null, 2)}
+          color="whiteAlpha.800"
+        >
+          {Array.from(state.tags).join(', ') ?? '&nbsp;'}
+        </Text>
+      </Box>
+    </ChakraProvider>
   );
 };
