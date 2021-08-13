fd330c9ec77707b63a444e7824a1d0356762c679
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-08-13 15:18:47 +0200

    Make changes to vercel-ignore.sh more readable

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -3,13 +3,22 @@
 # This script tells vercel which branches to build,
 # and which to ignore
 
+function get_branch_hash () {
+  local result="$(git rev-parse --short $1)"
+  echo "$result"
+}
+
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
+echo "main hash: $(get_branch_hash main)"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
   echo "✅ - Build can proceed"
   exit 1;
-elif [[ "$VERCEL_GIT_COMMIT_SHA" == "$(git rev-parse --short main)" ]] ; then
+# check if the current hash matches the tip of the main branch
+# multiple branches can point to a single hash
+# this tries to avoid a situation when a build targeting main gets skipped because a feature branch got assigned to VERCEL_GIT_COMMIT_REF
+elif [[ "$VERCEL_GIT_COMMIT_SHA" == "$(get_branch_hash main)" ]] ; then
   echo "✅ - Build can proceed"
   exit 1;
 else
