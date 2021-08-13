fd6360a3e4439eb643c60de78099c2ceb0ae2687
Santi <scros97@gmail.com>
2021-08-13 18:15:44 +0200

    Move event to useEffect and add e.preventDefault()
    Adding e.preventDefault on the current onWheel did not work :(

--- src/CanvasContainer.tsx
+++ src/CanvasContainer.tsx
@@ -182,35 +182,48 @@ export const CanvasContainer: React.FC = ({ children }) => {
     };
   }, [canvasService]);
 
+  /**
+   * Tracks Wheel Event on canvas
+   */
+  useEffect(() => {
+    const onCanvasWheel = (e: WheelEvent) => {
+      if (isMac() ? e.metaKey : e.ctrlKey) {
+        e.preventDefault();
+        if (e.deltaY > 0) {
+          canvasService.send(
+            canvasModel.events['ZOOM.OUT'](
+              e.clientX,
+              e.clientY,
+              ZoomFactor.slow,
+            ),
+          );
+        } else if (e.deltaY < 0) {
+          canvasService.send(
+            canvasModel.events['ZOOM.IN'](
+              e.clientX,
+              e.clientY,
+              ZoomFactor.slow,
+            ),
+          );
+        }
+      } else if (!e.metaKey && !e.ctrlKey) {
+        canvasService.send(canvasModel.events.PAN(e.deltaX, e.deltaY));
+      }
+    };
+
+    const canvasEl = canvasRef.current;
+    canvasEl.addEventListener('wheel', onCanvasWheel);
+    return () => {
+      canvasEl.removeEventListener('wheel', onCanvasWheel);
+    };
+  }, [canvasService]);
+
   return (
     <div
       ref={canvasRef}
       style={{
         cursor: getCursorByState(state),
         WebkitFontSmoothing: 'auto',
-      }}
-      onWheel={(e) => {
-        if (isMac() ? e.metaKey : e.ctrlKey) {
-          if (e.deltaY > 0) {
-            canvasService.send(
-              canvasModel.events['ZOOM.OUT'](
-                e.clientX,
-                e.clientY,
-                ZoomFactor.slow,
-              ),
-            );
-          } else if (e.deltaY < 0) {
-            canvasService.send(
-              canvasModel.events['ZOOM.IN'](
-                e.clientX,
-                e.clientY,
-                ZoomFactor.slow,
-              ),
-            );
-          }
-        } else if (!e.metaKey && !e.ctrlKey) {
-          canvasService.send(canvasModel.events.PAN(e.deltaX, e.deltaY));
-        }
       }}
       onPointerDown={(e) => {
         if (state.nextEvents.includes('GRAB')) {
