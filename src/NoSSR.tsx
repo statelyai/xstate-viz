1036e2a29ee1688ac3e9daf207814cfcb0aa83cc
Matt Pocock <mpocock@me.com>
2021-08-24 11:04:17 +0100

    Delete NoSSR.tsx

--- src/NoSSR.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { isOnClientSide } from './isOnClientSide';
-
-export const NoSSR: React.FC = ({ children }) => {
-  if (isOnClientSide()) return <>{children}</>;
-  return null;
-};
