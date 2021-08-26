4479409fe9ae1e02947aec7f9a285a5f916a2c51
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-08-26 12:10:17 +0200

    Add tests for the codec

--- src/localCache.ts
+++ src/localCache.ts
@@ -1,105 +1,11 @@
 import { isAfter } from 'date-fns';
 import { createStorage, testStorageSupport } from 'memory-web-storage';
 import { ThemeName, themes } from './editor-themes';
+import * as codec from './codec';
 
 export const storage = testStorageSupport()
   ? window.localStorage
   : createStorage();
-
-interface InternalCodec<T> {
-  __decode: Codec<T>['decode'];
-  __encode: Codec<T>['encode'];
-}
-
-interface Codec<T> {
-  /**
-   * this tries to match **parsed** value to the expected type
-   */
-  decode: (val: unknown) => T;
-  /**
-   * as encoded values are coming from the app we can trust our types
-   * this is used mainly to purify the value, pick only required keys and so on
-   */
-  encode: (val: T) => T;
-}
-
-function identity<T>(a: T): T {
-  return a;
-}
-
-function str(): InternalCodec<string> {
-  return {
-    __decode: (val: unknown) => {
-      if (typeof val !== 'string') {
-        throw new Error();
-      }
-      return val;
-    },
-    __encode: identity,
-  };
-}
-
-function num(): InternalCodec<number> {
-  return {
-    __decode: (val: unknown) => {
-      if (typeof val !== 'number') {
-        throw new Error();
-      }
-      return val;
-    },
-    __encode: identity,
-  };
-}
-
-function createCodec<T>() {
-  return (codec: InternalCodec<T>): Codec<T> => ({
-    decode: codec.__decode,
-    encode: codec.__encode,
-  });
-}
-
-type UnknownShape = Record<string, InternalCodec<any>>;
-
-type ShapeToObj<T extends UnknownShape> = {
-  [K in keyof T]: ReturnType<T[K]['__decode']>;
-};
-
-function obj<S extends UnknownShape>(shape: S): InternalCodec<ShapeToObj<S>> {
-  return {
-    __decode: (val: unknown) => {
-      if (typeof val !== 'object') {
-        throw new Error(`Input value is not an object.`);
-      }
-      if (val === null) {
-        throw new Error(`Input value is \`null\`.`);
-      }
-
-      const decoded: any = {};
-
-      for (const key of Object.keys(shape)) {
-        if (!(key in val)) {
-          throw new Error(
-            `Key "${key}" is missing in the expected shape of \`{${Object.keys(
-              shape,
-            ).join(', ')}}\``,
-          );
-        }
-        decoded[key] = shape[key].__decode((val as any)[key]);
-      }
-
-      return decoded;
-    },
-    __encode: (val: ShapeToObj<S>): ShapeToObj<S> => {
-      const encoded: any = {};
-
-      for (const key of Object.keys(shape)) {
-        encoded[key] = shape[key].__encode(val[key]);
-      }
-
-      return encoded;
-    },
-  };
-}
 
 export interface CachedPosition {
   zoom: number;
@@ -124,20 +30,20 @@ const makePositionCacheKey = (sourceID: string | null) =>
 const makeRawSourceCacheKey = (sourceID: string | null) =>
   `${RAW_SOURCE_CACHE_PREFIX}|${sourceID || 'no_source'}`;
 
-const cachedPositionCodec = createCodec<CachedPosition>()(
-  obj({
-    zoom: num(),
-    pan: obj({
-      dx: num(),
-      dy: num(),
+const cachedPositionCodec = codec.createCodec<CachedPosition>()(
+  codec.obj({
+    zoom: codec.num(),
+    pan: codec.obj({
+      dx: codec.num(),
+      dy: codec.num(),
     }),
   }),
 );
 
-const rawSourceCodec = createCodec<CachedSource>()(
-  obj({
-    sourceRawContent: str(),
-    date: str(),
+const rawSourceCodec = codec.createCodec<CachedSource>()(
+  codec.obj({
+    sourceRawContent: codec.str(),
+    date: codec.str(),
   }),
 );
 
