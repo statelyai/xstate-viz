4b1b768729351377ac9f0384d22dff1ebb724021
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-14 12:21:13 +0200

    wip

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,7 +1,4 @@
 import { DirectedGraphNode } from './directedGraph';
 import { useMachine, useSelector } from '@xstate/react';
 import { useEffect, useMemo, memo } from 'react';
-import { MachineViz } from './MachineViz';
-import { useCanvas } from './CanvasContext';
-import { useSimulation } from './SimulationContext';
 import { getAllEdges, StateElkNode } from './graphUtils';
