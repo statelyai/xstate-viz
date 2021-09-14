7e817d08b1273947512ca700a64356b05cfe8330
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-14 12:28:32 +0200

    wip

--- src/directedGraph.ts
+++ src/directedGraph.ts
@@ -9,21 +9,8 @@ export function toDirectedGraph(stateNode: StateNode): void {
     stateNode.transitions.map((t, transitionIndex) => {
       const targets = t.target ? t.target : [stateNode];
 
-      return targets.map((target, targetIndex) => {
-        const edge = new DirectedGraphEdge({
-          id: `${stateNode.id}:${transitionIndex}:${targetIndex}`,
-          source: stateNode,
-          target,
-          transition: t,
-          label: {
-            text: t.eventType,
-            x: 0,
-            y: 0,
-          },
-          sections: [],
-        });
-
-        return edge;
+      targets.map((target, targetIndex) => {
+        new DirectedGraphEdge();
       });
     }),
   );
