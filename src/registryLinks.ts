147b1c6e36bf528906cac7b978303db788f85274
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-14 11:53:42 +0200

    wip

--- src/registryLinks.ts
+++
@@ -1,8 +0,0 @@
-export const registryLinks = {
-  viewUserById: (id: string) =>
-    `${process.env.NEXT_PUBLIC_REGISTRY_PUBLIC_URL}/user/${id}`,
-  editSourceFile: (id: string) =>
-    `${process.env.NEXT_PUBLIC_REGISTRY_PUBLIC_URL}/file/${id}/edit`,
-  sourceFileOgImage: (id: string) =>
-    `${process.env.NEXT_PUBLIC_REGISTRY_PUBLIC_URL}/machines/${id}.png`,
-};
