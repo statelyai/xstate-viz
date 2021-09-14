bcb432fb79b023f17e42169721a4ab2d234a08df
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-14 12:10:03 +0200

    wip

--- src/PaletteContext.ts
+++
@@ -1,7 +0,0 @@
-import { InterpreterFrom } from 'xstate';
-import { paletteMachine } from './paletteMachine';
-import { createRequiredContext } from './utils';
-
-export const [PaletteProvider, usePalette] = createRequiredContext<
-  InterpreterFrom<typeof paletteMachine>
->('Palette');
