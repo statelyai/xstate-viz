c2187d319a9e8831d87d80433f592de9c8d9804a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-14 12:07:01 +0200

    wip

--- src/graphUtils.ts
+++ src/graphUtils.ts
@@ -1,13 +1,1 @@
-import {
-  DigraphBackLinkMap,
-  DirectedGraphEdge,
-  DirectedGraphNode,
-  getBackLinkMap,
-} from './directedGraph';
-import type {
-  ELK,
-  ElkEdgeSection,
-  ElkExtendedEdge,
-  ElkNode,
-  LayoutOptions,
-} from 'elkjs/lib/main';
+import './directedGraph';
