e4a28f27ad7578e416a80ff09876d20f0e5fd714
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-14 12:18:01 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,1 @@
 import './Graph';
-import { isOnClientSide } from './isOnClientSide';
-import { MachineNameChooserModal } from './MachineNameChooserModal';
-import { PaletteProvider } from './PaletteContext';
-import { paletteMachine } from './paletteMachine';
-import { PanelsView } from './PanelsView';
-import { SimulationProvider } from './SimulationContext';
-import { simulationMachine } from './simulationMachine';
-import { getSourceActor } from './sourceMachine';
-import { theme } from './theme';
-import { EditorThemeProvider } from './themeContext';
-import { EmbedContext, EmbedMode } from './types';
-import { useInterpretCanvas } from './useInterpretCanvas';
-import { useRouter } from 'next/router';
-import { parseEmbedQuery, withoutEmbedQueryParams } from './utils';
