2b145659c0f39f7566fda98cd37da08fa92f64bb
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-14 12:30:58 +0200

    wip

--- src/directedGraph.ts
+++ src/directedGraph.ts
@@ -1,17 +1,9 @@
 import { StateNode } from 'xstate';
 
-export class DirectedGraphNode {
-  public data: StateNode;
-}
-
 export function toDirectedGraph(stateNode: StateNode): void {
-  flatten(
-    stateNode.transitions.map((t, transitionIndex) => {
-      const targets = t.target ? t.target : [stateNode];
+  stateNode.transitions.map((t) => {
+    const targets = t.target ? t.target : [stateNode];
 
-      targets.map((target, targetIndex) => {
-        new DirectedGraphEdge();
-      });
-    }),
-  );
+    targets.map((target) => {});
+  });
 }
