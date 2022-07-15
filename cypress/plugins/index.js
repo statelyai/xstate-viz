e8b97a19b0936ccc5e7910d6823e83b435683a0c
Jaril <jarilvalenciano@gmail.com>
2022-07-15 12:58:30 -0700

    Record Cypress tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,7 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+const cypressReplay = require("@replayio/cypress");
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -19,4 +19,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  cypressReplay.default(on, config);
+  return config;
 }
