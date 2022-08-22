e609c523065a412c66907a0347165c2b48f80557
Anders Bech Mellson <mellson@icloud.com>
2022-08-22 16:34:00 +0200

    Update Auto import name and smarter test import

--- cypress/integration/autocomplete.spec.ts
+++ cypress/integration/autocomplete.spec.ts
@@ -1,3 +1,15 @@
+const importFromXstate = () => {
+  cy.get('body').then(($body) => {
+    if ($body.text().includes("Auto import from 'xstate'")) {
+      cy.getMonacoEditor().type('{enter}');
+    } else {
+      cy.getMonacoEditor().type('{downarrow}');
+      cy.wait(500);
+      importFromXstate();
+    }
+  });
+};
+
 describe('Autocomplete in the editor', () => {
   it('Should allow you to autocomplete XState imports', () => {
     cy.visit('/viz');
@@ -6,7 +18,7 @@ describe('Autocomplete in the editor', () => {
     // Wait for the autocomplete to show up
     cy.contains('assign');
 
-    cy.getMonacoEditor().type('{downarrow}{downarrow}{downarrow}{enter}');
+    importFromXstate();
 
     cy.contains(`import { assign, createMachine } from 'xstate';`);
   });
