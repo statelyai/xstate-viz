32c1bd86e33cec14171953c95a85423768e6bc54
Kevin Maes <kevin@kevinmaes.com>
2023-07-16 08:57:06 +0200

    Add once util function

--- src/utils.ts
+++ src/utils.ts
@@ -441,3 +441,15 @@ export const isErrorWithMessage = (
   error !== null &&
   'message' in error &&
   typeof (error as Record<string, unknown>).message === 'string';
+
+export function once<T extends (...args: any[]) => any>(fn: T) {
+  let called = false;
+  let result: ReturnType<T>;
+  return (...args: Parameters<T>) => {
+    if (!called) {
+      result = fn(...args);
+      called = true;
+    }
+    return result;
+  };
+}
